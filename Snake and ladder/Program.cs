90102a49cce89f38c834629c0241f531b015061a
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-15 17:01:27 +0530

    [rajaganapathi] adding s&L

---
+++ Snake and ladder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press anything to start");
+            Console.ReadLine();
+
+            int Pl1_position = 0; // player's starting position
+            int Pl1_numberOfRolls = 0; // number of times the dice was rolled
+            Random random = new Random();
+
+            int Pl1_diceRoll = random.Next(1, 7);
+            Pl1_numberOfRolls++;
+
+            Console.WriteLine("Dice for Player 1 Rolled: " + Pl1_diceRoll);
+
+            int Pl1_option = random.Next(0, 3);
+            switch (Pl1_option)
+            {
+                case 0: // No Play
+                    Console.WriteLine("No Play");
+                    break;
+                case 1: // Ladder
+                    Pl1_position += Pl1_diceRoll;
+                    Console.WriteLine("Ladder!");
+                    break;
+                case 2: // Snake
+                    Pl1_position -= Pl1_diceRoll;
+                    Console.WriteLine("Snake!!");
+                    break;
+            }
+
+            Console.WriteLine("The position for Player 1 is : " + Pl1_position);
+
+        }
+    }
+}
